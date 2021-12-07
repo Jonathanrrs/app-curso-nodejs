172624de2d1686c7fd671564ab7167cfe01fd719
jonathan <yoni_12rrs@hotmail.com>
2021-12-06 21:08:48 -0600

    Retornar número total de registros en una colección

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,16 +4,29 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 
-
 const usuariosGet = async(req = request, res = response) => {
 
     /* limite que viene de la url get ?limite=algo */
     const {limite = 5, desde = 0} = req.query;
-    const usuarios = await Usuario.find()
-    .skip(Number(desde))
-        .limit(Number(limite))
+    const query = {estado: true}
+
+    // const usuarios = await Usuario.find(query)
+    //     .skip(Number(desde))
+    //     .limit(Number(limite))
+
+    // const total = await Usuario.countDocuments(query);
+
+    /* await para que espere la resolución de ambas promesas */
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+
+    ]);
 
     res.json({
+        total,
         usuarios
     });
 };
