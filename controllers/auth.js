3275c568833c92fde8ea758af9ea02a74537a1ec
jonathan <yoni_12rrs@hotmail.com>
2021-12-08 20:59:49 -0600

    Crear un usuario personalizado con las credenciales de Googl

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,14 +57,41 @@ const login = async (req, res = response) => {
 
 const googleSignIn = async (req, res = response) => {
     const { id_token } = req.body;
-
+    
     try {
 
-        const googleUser = await googleVerify(id_token);
-        console.log(googleUser);
+        const {nombre, img, correo} = await googleVerify(id_token);
+        // const googleUser = await googleVerify(id_token);
+        // console.log(googleUser);
+        let usuario = await Usuario.findOne({correo});
+        
+        if(!usuario) {
+            /* se debe crear */
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        /* si el usuario en DB */
+        if(!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // /* Generar el JWT */
+        const token = await generarJWT(usuario.id)
+
         res.json({
-            msg: 'Todo bien',
-            id_token
+            usuario,
+            token
         });
     } catch (error) {
         res.status(400).json({
