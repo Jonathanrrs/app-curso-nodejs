1e644a8ecb96b280df1b18ef1ea9f02551bf08e6
jonathan <yoni_12rrs@hotmail.com>
2021-12-10 14:59:18 -0600

    Crear una categoria

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,7 +1,10 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT, validarCampos } = require('../middlewares');
+
+const { crearCategoria } = require('../controllers/categorias');
+
 
 const router = Router();
 
@@ -18,9 +21,11 @@ router.get('/:id', (req,res) => {
 });
 
 /* Crear categoria- privado(cualquier persona con un token valido) */
-router.post('/', (req,res) => {
-    res.json('post');
-});
+router.post('/', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos
+],crearCategoria);
 
 /* Actualizar - privado(cualquiera con token válido) */
 router.put('/:id', (req,res) => {
