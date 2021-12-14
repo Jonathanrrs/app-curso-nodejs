1ab020a6ced7b7e5a9c7b126f24d68c03dba9b08
jonathan <yoni_12rrs@hotmail.com>
2021-12-13 21:29:20 -0600

    Búsquedas en base de datos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,13 +1,49 @@
 const { response, request } = require("express");
+const { ObjectId } = require("mongoose").Types;
+
+const {Usuario, Categoria, Producto} = require('../models');
+
+const coleccionesPermitidas = [
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles',
+];
+
+const buscarUsuarios = async(termino='', res = response) => {
+    const esMongoId = ObjectId.isValid(termino); /* true */
+
+    if(esMongoId) {
+        const usuario = await Usuario.findById(termino);
+        res.json({
+            results: (usuario) ? [usuario] : []
+        })
+    }
+}
 
 const buscar = (req = request, res = response) => {
 
     const {coleccion, termino} = req.params;
 
-    res.json({
-        coleccion,
-        termino
-    })
+    if(!coleccionesPermitidas.includes(coleccion)) {
+        return res.status(404).json({
+            msg: `Las colecciones permitidas son: ${coleccionesPermitidas}`
+        })
+    }
+
+    switch(coleccion) {
+        case 'usuarios':
+            buscarUsuarios(termino, res);
+        break;
+        case 'categoria':
+            break;
+        case 'productos':
+            break;
+        default:
+            res.status(500).json({
+                msg: 'Se le olvidó hacer esta búsqueda'
+            })
+    }
 }
 
 module.exports = {
