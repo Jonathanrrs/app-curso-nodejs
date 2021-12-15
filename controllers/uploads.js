9755a2cbc8eb2b4116d98661073baf33a47a9668
jonathan <yoni_12rrs@hotmail.com>
2021-12-15 13:02:59 -0600

    Validar la extensión

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -10,16 +10,29 @@ const cargarArchivo = (req, res = response) => {
     }
 
     const {archivo} = req.files;
-    const uploadPath = path.join(__dirname, '/../uploads/', archivo.name);
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length-1];
+
+    /* Valiar extensión */
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if(!extensionesValidas.includes(extension)) {
+        return res.status(400).json({
+            msg: `La extensión ${extension} no es permitida, ${extensionesValidas}`
+        })
+    }
+
+    res.json({extension})
+
+    // const uploadPath = path.join(__dirname, '/../uploads/', archivo.name);
+
+    // archivo.mv(uploadPath, (err) => {
+    //   if (err) {
+    //       return res.status(500).json({err});
+    //   }
   
-    // Use the mv() method to place the file somewhere on your server
-    archivo.mv(uploadPath, (err) => {
-      if (err) {
-          return res.status(500).json({err});
-      }
-  
-      res.json({msg: 'File uploaded to' + uploadPath});
-    });
+    //   res.json({msg: 'File uploaded to' + uploadPath});
+    // });
 
 }
 
