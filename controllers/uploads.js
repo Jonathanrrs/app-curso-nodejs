b3e7f90493b27c278d2931e3a5b732abd7b03a80
jonathan <yoni_12rrs@hotmail.com>
2021-12-15 16:27:00 -0600

    Crear carpetas de destino

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -4,21 +4,32 @@ const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 
 const cargarArchivo = async (req, res = response) => {
-    
-  
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-      return res.status(400).json({msg: 'No hay archivos que subir'});
-    }
 
-    /* Imágenes */
-    const nombre = await subirArchivo(req.files);
 
-    res.json({nombre})
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    return res.status(400).json({ msg: 'No hay archivos que subir' });
+  }
 
-    
+  // /* Imágenes */
+  // const nombre = await subirArchivo(req.files);
+
+  try {
+    // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
+    const nombre = await subirArchivo(req.files, undefined, 'imgs');
+    res.json({ nombre })
+  } catch (msg) {
+    res.status(400).json({ msg })
+  }
+
+
+
+
+  res.json({ nombre })
+
+
 
 }
 
 module.exports = {
-    cargarArchivo
+  cargarArchivo
 }
